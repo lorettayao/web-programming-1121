c560992a8ed6137ab197e2d7ac130695f1cdc221
Loretta Yao <loret@Lorettas-MacBook-Air.local>
2023-09-25 14:11:39 +0800

    new edition

--- hw1/frontend/func.js
+++
@@ -1,4 +0,0 @@
-function add_func(){
-    var add_b=document.getElementById("1234");
-    document.getElementById("todo-item-template").appendChild(add_b);
-}
--- hw1/frontend/index.js
+++ hw1/frontend/index.js
@@ -1,51 +1,36 @@
+/* global axios */
 const itemTemplate = document.querySelector("#todo-item-template");
 const todoList = document.querySelector("#todos");
-const apiRoot = "http://localhost:8000/api";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8000/api",
+});
 
 async function main() {
-  
-  updateCurrentDateTime();
-  setInterval(updateCurrentDateTime, 1000);
   setupEventListeners();
-  console.log("hi");
   try {
     const todos = await getTodos();
     todos.forEach((todo) => renderTodo(todo));
-    // console.log(todo);
   } catch (error) {
-    alert("Failed to load todos! here");
+    alert("Failed to load todos!");
   }
 }
-// add func for showing date
-function updateCurrentDateTime() {
-  const dateTimeElement = document.getElementById("current-date-time");
-  const currentDate = new Date();
-  const options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric"
-  };
-  const formattedDate = currentDate.toLocaleDateString("en-US", options);
-  dateTimeElement.textContent = "Current Date: " + formattedDate;
-}
-
 
 function setupEventListeners() {
   const addTodoButton = document.querySelector("#todo-add");
   const todoInput = document.querySelector("#todo-input");
   const todoDescriptionInput = document.querySelector(
-    "#todo-description-input",
+    "#todo-description-input"
   );
-  const categorySelect=document.querySelector("#todo-category");
-  const moodSelect=document.querySelector("#todo-mood");
-  const dateuse = document.querySelector("#todo-date");
-  
+  const option1 =document.querySelector("#option1-input");
+  const option2 =document.querySelector("#option2-input");
+  // const showdate=document.querySelector("p.showdate")
   addTodoButton.addEventListener("click", async () => {
     const title = todoInput.value;
     const description = todoDescriptionInput.value;
-    const category=categorySelect.value;
-    const mood=moodSelect.value;
-    const dateuse=updateCurrentDateTime();
+    const write_op1 = option1.value;
+    const write_op2 = option2.value;
+    // console.log("write_2 vlaue:",write_op2);
     if (!title) {
       alert("Please enter a todo title!");
       return;
@@ -55,7 +40,8 @@ function setupEventListeners() {
       return;
     }
     try {
-      const todo = await createTodo({ title, description,category,mood,dateuse });
+      const todo = await createTodo({ title, description, write_op1, write_op2 });
+      console.log(write_op1);
       renderTodo(todo);
     } catch (error) {
       alert("Failed to create todo!");
@@ -68,6 +54,7 @@ function setupEventListeners() {
 
 function renderTodo(todo) {
   const item = createTodoElement(todo);
+  console.log("render",todo);
   todoList.appendChild(item);
 }
 
@@ -75,37 +62,38 @@ function createTodoElement(todo) {
   const item = itemTemplate.content.cloneNode(true);
   const container = item.querySelector(".todo-item");
   container.id = todo.id;
+  console.log(todo);
   const checkbox = item.querySelector(`input[type="checkbox"]`);
   checkbox.checked = todo.completed;
   checkbox.dataset.id = todo.id;
   const title = item.querySelector("p.todo-title");
   title.innerText = todo.title;
-  // add catagory and mood
-
-  const category = item.querySelector("p.todo-category");
-  category.textContent = todo.category;
-  
-  const mood = item.querySelector("p.todo-mood");
-  mood.textContent = todo.mood;
-  //改成textcontent from value it works!
-  // must create p. for above, or the todo cannot load
-  const dateuse = item.querySelector("p.todo-date");
-  dateuse.value= updateCurrentDateTime();
-  
   const description = item.querySelector("p.todo-description");
   description.innerText = todo.description;
+  console.log(todo);
+  const option1 = item.querySelector("p.todo-option1");
+  option1.innerText= todo.write_op1;
+  console.log(todo);
+  const option2 = item.querySelector("p.todo-option2");
+  option2.innerText = todo.write_op2;
+  console.log(todo);
+  console.log(todo.write_op1);
+
+
+
+const currentDate = new Date().toLocaleDateString();
+
+// Select the .showdate element within the cloned item
+const showDateElement = item.querySelector(".showdate");
+
+// Set the text content of the .showdate element to the formatted date
+showDateElement.textContent = currentDate;
+
   const deleteButton = item.querySelector("button.delete-todo");
   deleteButton.dataset.id = todo.id;
-  const editButton = item.querySelector("button.edit-todo");
-  editButton.dataset.id = todo.id;
   deleteButton.addEventListener("click", () => {
     deleteTodoElement(todo.id);
   });
-  editButton.addEventListener("click",(event) =>{
-    handleEditButtonClick(event);
-  }
-  
-  );
   return item;
 }
 
@@ -120,91 +108,25 @@ async function deleteTodoElement(id) {
   }
 }
 
-
-
-// Function to handle the edit button click event
-function handleEditButtonClick(event) {
-  const todoItem = event.target.closest(".todo-item");
-  if (!todoItem) { 
-    console.log("not inside handle function");
-  return;} 
-
-  const todoTitleElement = todoItem.querySelector(".todo-title");
-  const editTitleInput = todoItem.querySelector(".edit-title-input");
-  const todoDescriptionElement=todoItem.querySelector(".todo-description");
-  const editDescriptionInput=todoItem.querySelector(".edit-description-input");
-  const editButton = todoItem.querySelector(".edit-todo");
-
-  if (editTitleInput.style.display === "none") {
-    
-    todoTitleElement.style.display = "none";
-    editTitleInput.style.display = "inline"; 
-    editTitleInput.value = todoTitleElement.textContent; 
-    
-    todoDescriptionElement.style.display = "none";
-    editDescriptionInput.style.display = "inline"; 
-    editDescriptionInput.value = todoDescriptionElement.textContent; 
-    editButton.textContent = "Save"; 
-  } else {
-    const editedTitle = editTitleInput.value;
-    todoTitleElement.textContent = editedTitle;
-    editTitleInput.style.display = "none";
-    todoTitleElement.style.display = "inline"; 
-    const editedDescription = editDescriptionInput.value;
-    todoDescriptionElement.textContent = editedDescription;
-    editDescriptionInput.style.display = "none";
-    todoDescriptionElement.style.display = "inline"; 
-
-    editButton.textContent = "Edit"; 
-    
-  }
-}
-
-const editButtons = document.querySelectorAll(".edit-todo");
-editButtons.forEach((button) => {
-  button.addEventListener("click", handleEditButtonClick);
-});
-
-
 async function getTodos() {
-  const response = await fetch(`${apiRoot}/todos`);
-  const data = await response.json();
-  return data;
+  const response = await instance.get("/todos");
+  return response.data;
 }
 
 async function createTodo(todo) {
-  const response = await fetch(`${apiRoot}/todos`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(todo),
-  });
-  const data = await response.json();
-  
-  return data;
+  const response = await instance.post("/todos", todo);
+  return response.data;
 }
 
 // eslint-disable-next-line no-unused-vars
 async function updateTodoStatus(id, todo) {
-  const response = await fetch(`${apiRoot}/todos/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(todo),
-  });
-  const data = await response.json();
-  return data;
+  const response = await instance.put(`/todos/${id}`, todo);
+  return response.data;
 }
 
 async function deleteTodoById(id) {
-  const response = await fetch(`${apiRoot}/todos/${id}`, {
-    method: "DELETE",
-  });
-  const data = await response.json();
-  return data;
+  const response = await instance.delete(`/todos/${id}`);
+  return response.data;
 }
-
 
 main();
