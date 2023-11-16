dbccae17174e4901061a6fda013978eebae5b113
lorettayao <a0910330206@gmail.com>
2023-11-17 07:39:53 +0800

    it store the content when switch

--- hw4/src/hooks/useDocument.ts
+++ hw4/src/hooks/useDocument.ts
@@ -1,17 +1,57 @@
-import { useState,useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import { useParams ,useRouter} from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
-import type { Document } from "@/lib/types/db";
 import { useDebounce } from "use-debounce";
 
+import type { Document } from "@/lib/types/db";
 
 export const useDocument = () => {
-    const { docId } = useParams();
-    const documentId = Array.isArray(docId) ? docId[0] : docId;
+  const { docId } = useParams();
+  const documentId = Array.isArray(docId) ? docId[0] : docId;
+
   const [document, setDocument] = useState<Document | null>(null);
   const [debouncedDocument] = useDebounce(document, 1000);
   const router = useRouter();
+
+  // When the debounced document changes, update the document
+  useEffect(() => {
+    const updateDocument = async () => {
+      if (!debouncedDocument) return;
+      const res = await fetch(`/api/documents/${documentId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: debouncedDocument.title,
+          content: debouncedDocument.content,
+        }),
+      });
+      if (!res.ok) {
+        return;
+      }
+      // Update the navbar
+      router.refresh();
+    };
+    updateDocument();
+  }, [debouncedDocument, documentId, router]);
+
+  useEffect(() => {
+    if (!documentId) return;
+    const fetchDocument = async () => {
+      const res = await fetch(`/api/documents/${documentId}`);
+      if (!res.ok) {
+        setDocument(null);
+        router.push("/docs");
+        return;
+      }
+      const data = await res.json();
+      setDocument(data);
+    };
+    fetchDocument();
+  }, [documentId, router]);
+
   const title = document?.title || "";
   const setTitle = (newTitle: string) => {
     if (document === null) return;
@@ -29,31 +69,10 @@ export const useDocument = () => {
       content: newContent,
     });
   };
-  useEffect(() => {
-    console.log("=======");
-  }, [document]);
-  useEffect(() => {
-    console.log("*******");
-  }, [debouncedDocument]);
-  useEffect(() => {
-    if (!documentId) return;
-    const fetchDocument = async () => {
-      const res = await fetch(`/api/documents/${documentId}`);
-      if (!res.ok) {
-        setDocument(null);
-        router.push("/docs");
-        return;
-      }
-      const data = await res.json();
-      setDocument(data);
-    };
-    fetchDocument();
-  }, [documentId, router]);
 
   return {
     documentId,
     document,
-    // setDocument,
     title,
     setTitle,
     content,
