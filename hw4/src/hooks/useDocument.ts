235da4c494b3c84c7ab7f143030b09a3a0b6bd03
lorettayao <a0910330206@gmail.com>
2023-11-17 07:23:49 +0800

    at this stage still cannot store the title and content

--- hw4/src/hooks/useDocument.ts
+++ hw4/src/hooks/useDocument.ts
@@ -3,11 +3,38 @@ import { useState,useEffect } from "react";
 import { useParams ,useRouter} from "next/navigation";
 
 import type { Document } from "@/lib/types/db";
+import { useDebounce } from "use-debounce";
+
 
 export const useDocument = () => {
-  const { docId: documentId } = useParams();
+    const { docId } = useParams();
+    const documentId = Array.isArray(docId) ? docId[0] : docId;
   const [document, setDocument] = useState<Document | null>(null);
+  const [debouncedDocument] = useDebounce(document, 1000);
   const router = useRouter();
+  const title = document?.title || "";
+  const setTitle = (newTitle: string) => {
+    if (document === null) return;
+    setDocument({
+      ...document,
+      title: newTitle,
+    });
+  };
+
+  const content = document?.content || "";
+  const setContent = (newContent: string) => {
+    if (document === null) return;
+    setDocument({
+      ...document,
+      content: newContent,
+    });
+  };
+  useEffect(() => {
+    console.log("=======");
+  }, [document]);
+  useEffect(() => {
+    console.log("*******");
+  }, [debouncedDocument]);
   useEffect(() => {
     if (!documentId) return;
     const fetchDocument = async () => {
@@ -22,10 +49,14 @@ export const useDocument = () => {
     };
     fetchDocument();
   }, [documentId, router]);
-  
+
   return {
     documentId,
     document,
     // setDocument,
+    title,
+    setTitle,
+    content,
+    setContent,
   };
 };
