35a2055dd5ef9530129d428c1d5779a6bcccb4dc
lorettayao <a0910330206@gmail.com>
2023-11-23 09:52:39 +0800

    0952

---
+++ hack2/src/app/projects/_components/SignOutButton.tsx
@@ -0,0 +1,19 @@
+// TODO: 4. Call the signOut() function when the button is clicked
+// hint: You may want to change the first line of this file
+"use client";
+import { signOut } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+
+
+export default function SignOutButton() {
+  const handleSignOut = async () => {
+    await signOut(); // Call the signOut function from next-auth/react to sign out the user
+  };
+  return (
+    <Button variant={"outline"} onClick={handleSignOut}>
+      Sign Out
+    </Button>
+  );
+  }
+// TODO: 4. end
