a4ee406f147390d9d6127961f17eefbd5859f9b7
lorettayao <a0910330206@gmail.com>
2023-12-30 20:09:31 +0800

    clear

--- final/nextauth.d.ts
+++
@@ -1,9 +0,0 @@
-import type { DefaultSession } from "next-auth";
-
-import type { User } from "@/lib/types";
-
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user?: User;
-  }
-}
